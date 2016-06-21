ece4b35a902b77eea9c20e9a9cee4f0028d1e2f8
Homura <ywz.believe@qq.com>
2016-06-22 01:18:28 +0800

    Add files via upload

---
+++ final-exam/模拟1.py
@@ -0,0 +1,28 @@
+import random
+
+door1 = 1
+door2 = 2
+door3 = 3
+j=0.0
+
+pick1 = raw_input("please choose one of the doors: ")
+pick1 = int(pick1)
+
+for i in range(10000):
+
+    car = random.randint(1,3)
+
+    while 1:
+	  kick1 = random.randint(1,3)
+	  kick1 = int(kick1)
+	  if kick1 != car and kick1 != pick1:
+
+		pick2 = int(pick1)
+		if pick2 == car:
+			j=j+1
+			break
+		else:
+			break
+
+k=j/10000
+print k
