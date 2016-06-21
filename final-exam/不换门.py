33cbc8b1797f1498090257704867a2a04bc5b622
Homura <ywz.believe@qq.com>
2016-06-22 07:37:27 +0800

    Add files via upload

---
+++ final-exam/不换门.py
@@ -0,0 +1,30 @@
+import random
+from numpy import *
+
+n=int(raw_input("please input the number of the doors: "))
+a=range(1,n+1)
+
+j=0.0
+
+pick1 = raw_input("please choose one of the doors: ")
+pick1 = int(pick1)
+
+for i in range(10000):
+
+    from random import choice
+    car = choice(a)
+
+    while 1:
+	  kick1 = choice(a)
+          kick1=int(kick1)
+	  if kick1 != car and kick1 != pick1:
+
+		pick2 = int(pick1)
+		if pick2 == car:
+			j=j+1
+			break
+		else:
+			break
+
+k=j/10000
+print k
---
+++ final-exam/换门1.py
@@ -0,0 +1,39 @@
+import random
+
+m=[]
+n=int(raw_input("please input the number of the cars: " ))
+
+l=0.0
+
+pick1 = raw_input("please choose one of the doors: ")
+pick1 = int(pick1)
+
+for i in range(10000):
+
+    m=[]
+    while n!=len(m):
+        x=random.randint(1,10)
+        if x in m:
+            continue
+        else:
+            m.append(x)
+
+    while 1:
+	kick1 = random.randint(1,10)
+        kick1=int(kick1)
+
+        if kick1 not in m and kick1 != pick1:
+
+            a=range(1,11)
+            a.remove(pick1)
+            a.remove(kick1)
+            from random import choice
+	    pick2 = choice(a)
+            if pick2 in m:
+        	l=l+1
+		break
+	    else:
+		break
+
+k=l/10000
+print k
